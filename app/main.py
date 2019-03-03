c024a0798f46c13e263f81e52cff671b91adfdf2
scsukas8 <sean.csukas@gmail.com>
2019-03-03 14:33:22 -0800

    Added deep learning main

--- app/main.py
+++ app/main.py
@@ -6,91 +6,114 @@ import bottle
 
 from api import ping_response, start_response, move_response, end_response
 
+#import os,sys,inspect
+#currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+#parentdir = os.path.dirname(currentdir)
+#sys.path.insert(0,parentdir)
 
-@bottle.route('/')
-def index():
-    return '''
-    Battlesnake documentation can be found at
-       <a href="https://docs.battlesnake.io">https://docs.battlesnake.io</a>.
-    '''
-
-@bottle.route('/static/<path:path>')
-def static(path):
-    """
-    Given a path, return the static file located relative
-    to the static folder.
-
-    This can be used to return the snake head URL in an API response.
-    """
-    return bottle.static_file(path, root='static/')
-
-@bottle.post('/ping')
-def ping():
-    """
-    A keep-alive endpoint used to prevent cloud application platforms,
-    such as Heroku, from sleeping the application instance.
-    """
-    return ping_response()
+#from alpha_snake import Battlesnake
 
 
+class App():
 
-@bottle.post('/start')
-def start():
-    data = bottle.request.json
+    def __init__(self):
+        #self.battlesnake = Battlesnake()
 
-    """
-    TODO: If you intend to have a stateful snake AI,
-            initialize your snake state here using the
-            request's data if necessary.
-    """
-    print(json.dumps(data))
+    def run(self):
+        bottle.run(
+            application,
+            host=os.getenv('IP', '0.0.0.0'),
+            port=os.getenv('PORT', '8080'),
+            debug=os.getenv('DEBUG', True)
+        )
 
-    BOARD_HEIGHT = data[BOARD_KEY][HEIGHT_KEY]
-    BOARD_WIDTH = data[BOARD_KEY][WIDTH_KEY]
-    print(BOARD_WIDTH)
+    def index(self):
+        return '''
+        Battlesnake documentation can be found at
+           <a href="https://docs.battlesnake.io">https://docs.battlesnake.io</a>.
+        '''
 
-    color = "#342D7E"
+    def static(path):
+        """
+        Given a path, return the static file located relative
+        to the static folder.
 
-    return start_response(color)
+        This can be used to return the snake head URL in an API response.
+        """
+        return bottle.static_file(path, root='static/')
 
-@bottle.post('/move')
-def move():
-    data = bottle.request.json
-
-    """
-    TODO: Using the data from the endpoint request object, your
-            snake AI must choose a direction to move in.
-    """
-    print(json.dumps(data))
-
-    directions = ['up', 'down', 'left', 'right']
-
-    directions = [ d for d in directions if is_valid_action(d,data) ]
-
-    direction = random.choice(directions)
-
-    return move_response(direction)
+    def ping(self):
+        """
+        A keep-alive endpoint used to prevent cloud application platforms,
+        such as Heroku, from sleeping the application instance.
+        """
+        return ping_response()
 
 
-@bottle.post('/end')
-def end():
-    data = bottle.request.json
+    def start(self):
+        data = bottle.request.json
 
-    """
-    TODO: If your snake AI was stateful,
-        clean up any stateful objects here.
-    """
-    print(json.dumps(data))
+        """
+        TODO: If you intend to have a stateful snake AI,
+                initialize your snake state here using the
+                request's data if necessary.
+        """
+        print(json.dumps(data))
+        
+        #self.battlesnake.start(data)
+        print("Snek Started")
+        #action, _, _, _ =  self.battlesnake.act(data)
 
-    return end_response()
+
+        color = "#342D7E"
+
+        return start_response(color)
+
+
+    def move(self):
+        data = bottle.request.json
+
+        """
+        TODO: Using the data from the endpoint request object, your
+                snake AI must choose a direction to move in.
+        """
+        print(json.dumps(data))
+
+        directions = ['up', 'down', 'left', 'right']
+
+        #action, _, _, _ =  self.battlesnake.act(data)
+
+        direction = directions[0]#action]
+        print(direction)
+
+        return move_response(direction)
+
+
+    def end(self):
+        data = bottle.request.json
+
+        """
+        TODO: If your snake AI was stateful,
+            clean up any stateful objects here.
+        """
+        print(json.dumps(data))
+
+        return end_response()
+
+
 
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
 
+
+
+
 if __name__ == '__main__':
-    bottle.run(
-        application,
-        host=os.getenv('IP', '0.0.0.0'),
-        port=os.getenv('PORT', '8080'),
-        debug=os.getenv('DEBUG', True)
-    )
+    app = App()
+    bottle.route('/')(app.index)
+    bottle.route('/static/<path:path>')(app.static)
+    bottle.post('/ping')(app.ping)
+    bottle.post('/start')(app.start)
+    bottle.post('/move')(app.move)
+    bottle.post('/end')(app.end)
+    app.run()
